14aa280e7969bd41b84d4d926c7f2b27f3a6cc22
rafi993 <rafi993@protonmail.coml>
2024-05-27 10:01:47 +0530

    Adding harpoon

--- lua/config/plugins/harpoon.lua
+++ lua/config/plugins/harpoon.lua
@@ -9,20 +9,21 @@ return {
     harpoon:setup()
     -- REQUIRED
 
+    -- Add to harpoon
     vim.keymap.set('n', '<leader>a', function()
       harpoon:list():add()
     end)
-    vim.keymap.set('n', '<C-e>', function()
+    vim.keymap.set('n', '<C-l>', function()
       harpoon.ui:toggle_quick_menu(harpoon:list())
     end)
 
     vim.keymap.set('n', '<C-h>', function()
       harpoon:list():select(1)
     end)
-    vim.keymap.set('n', '<C-t>', function()
+    vim.keymap.set('n', '<C-j>', function()
       harpoon:list():select(2)
     end)
-    vim.keymap.set('n', '<C-n>', function()
+    vim.keymap.set('n', '<C-k>', function()
       harpoon:list():select(3)
     end)
     vim.keymap.set('n', '<C-s>', function()
