4c3914caf292039244209fc795c1dc9a9ef5a4f1
rafi993 <rafi993@protonmail.com>
2024-06-19 20:04:57 +0530

    Use ruff for python lsp

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -159,7 +159,8 @@ return {
     local servers = {
       -- clangd = {},
       -- gopls = {},
-      pyright = {},
+      ruff = {},
+
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
       --
