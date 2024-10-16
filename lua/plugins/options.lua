a06df612a861beaa5ebfbbb8b8a3d7a47276b762
rafi993 <rafi993@protonmail.com>
2024-10-16 10:58:05 +0530

    Adding keyboard shortcut for neotree

--- lua/plugins/options.lua
+++ lua/plugins/options.lua
@@ -30,7 +30,47 @@ if true then return {
       "neovim/nvim-lspconfig",
     },
     opts = {}
-   }
+   },
+   {
+    "nvim-neo-tree/neo-tree.nvim",
+    version = "*",
+    events = "VeryLazy",
+    dependencies = {
+     "nvim-lua/plenary.nvim",
+     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+     "MunifTanjim/nui.nvim",
+    },
+    cmd = "Neotree",
+    keys = {
+     { "\\", ":Neotree reveal<CR>", { desc = "NeoTree reveal" } },
+    },
+    opts = {
+    filesystem = {
+      window = {
+        position = "left",
+        mappings = {
+          ["\\"] = "close_window",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "+", -- or "✚", but this is redundant info if you use git_status_colors on the name
+            modified = "", -- or "", but this is redundant info if you use git_status_colors on the name
+            deleted = "✖", -- this can only be used in the git_status source
+            renamed = "󰁕", -- this can only be used in the git_status source
+            -- Status type
+            untracked = "",
+            ignored = "",
+            unstaged = "󰄱",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+    },
+  },
+}
+
 } end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
